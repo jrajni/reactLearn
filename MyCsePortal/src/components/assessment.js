f3a0fecfea02c25b7736143d08f07103d9c5b3bd
System Administrator <root@Arrs-MacBook-Air.local>
2019-11-06 10:20:09 +0530

    my-cse-portal

--- MyCsePortal/src/components/assessment.js
+++
@@ -1,7 +0,0 @@
-import React,{component} from 'react'
-function Practice(){
-    return(<div>
-        <h1>this is sample component of assessment</h1>
-    </div>)
-}
-export default Practice; 
--- MyCsePortal/src/components/faq.js
+++
@@ -1,7 +0,0 @@
-import React,{component} from 'react'
-function Practice(){
-    return(<div>
-        FAQ this is sample of FAQ component 
-    </div>)
-}
-export default Practice; 
